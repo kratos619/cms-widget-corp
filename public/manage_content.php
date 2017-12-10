0dd8ed08d728c4633598b46bdeb515d08a6ee9f8
kratos619 <gaurav.palaspagar@hotmail.com>
2017-12-10 10:53:05 +0530

    adding pages to the navigation subjects
    here am i display pages belong to the subjects....
    
    some changes i made $result is change to $subject_set

--- public/manage_content.php
+++ public/manage_content.php
@@ -5,42 +5,68 @@ require_once("../include/db_connection.php"); ?>
 <?php
 // include once tym
 require_once("../include/functions.php"); ?>
-
-<?php
-// 2. perform quey
-$query = "select * from subjects";
-$result = mysqli_query($connection, $query);
-  // test if there was a error
-	// calling custome functio confirm_query
-confirm_query($result);
-?>
-
-
 <?php include("../include/layouts/header.php"); ?>
 <div id="main">
 	<div id="navigation">
 		<ul class="subjects">
+			<?php
+			// 2. perform quey
+			$query = "select * ";
+			$query .= "from subjects ";
+			$query .= "where visible = 1 ";
+			$query .= "order by position ASC";
+			$subject_set = mysqli_query($connection, $query);
+			  // test if there was a error
+				// calling custome functio confirm_query
+			confirm_query($subject_set);
+			?>
       <?php
       //3. return data if any key are <integ></integ>er  mysqli_fetch_row
       // key are coloumn result are in an associative array mysqli_fetch_assoc
       // result in either or both typer of arrays mysqli_fetch_assoc
       //while($row = mysqli_fetch_row($result)){
-      while($subjects = mysqli_fetch_assoc($result)){
+      while($subject = mysqli_fetch_assoc($subject_set)){
       ?>
-      <li> <?php echo $subjects["menu_name"]; ?></li>
+      <li>
+				 <?php echo $subject["menu_name"]; ?>
+				 <?php
+	 			// 2. perform quey
+	 			$query = "select * ";
+				$query .= "from pages ";
+				$query .= "where visible = 1 ";
+				$query .="and subject_id = {$subject["id"]} ";
+				$query .= "order by position ASC";
+	 			$page_set = mysqli_query($connection, $query);
+	 			  // test if there was a error
+	 				// calling custome functio confirm_query
+	 			confirm_query($page_set);
+	 			?>
+				 <ul class="pages">
+					 <?php
+					 while($page = mysqli_fetch_assoc($page_set)){
+	 	      ?>
+					<li>
+						<?php echo $page["menu_name"]; ?>
+					</li>
+					<?php
+				}
+					 ?>
+					 <?php mysqli_free_result($page_set); ?>
+				 </ul>
+			</li>
 
       <?php
       }
        ?>
+			 <?php
+			 // 4 reslese retun data
+			 mysqli_free_result($subject_set);
+			  ?>
+
     </ul>
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
 	</div>
 </div>
-<?php
-// 4 reslese retun data
-mysqli_free_result($result);
- ?>
-
 <?php include("../include/layouts/footer.php"); ?>

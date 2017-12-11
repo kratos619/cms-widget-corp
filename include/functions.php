0de5dde5ec60a29b2c2ba3d1e15a43c7ca2310ee
kratos619 <gaurav.palaspagar@hotmail.com>
2017-12-11 14:17:58 +0530

    navigation function
    create function navigation
    the currently selected subect id
    // currently selected page id

--- include/functions.php
+++ include/functions.php
@@ -37,4 +37,58 @@ function find_pages_for_subjects($subject_id){
   return $page_set;
 }
 
- ?>
+//function navigation
+//the currently selected subect id
+// currently selected page id
+function navigation($subject_id,$page_id){
+  $output = '<ul class="subjects">';
+    // print subject by creating function see function.php
+    $subject_set =  find_all_subjects();
+    //3. return data if any key are <integ></integ>er  mysqli_fetch_row
+    // key are coloumn result are in an associative array mysqli_fetch_assoc
+    // result in either or both typer of arrays mysqli_fetch_assoc
+    //while($row = mysqli_fetch_row($result)){
+    while($subject = mysqli_fetch_assoc($subject_set)){
+        $output .= "<li";
+        if($subject["id"] == $subject_id){
+        $output .= " class=\"selected\"";
+        }
+        else{
+        $output .= ">";
+        }
+
+			$output .= '<a href="manage_content.php?subject=';
+			$output .= urlencode($subject["id"]);
+			$output .=  '">';
+				$output .= $subject["menu_name"];
+			$output .='</a>';
+       // function find pages belong to subjects subjects["id"] is belong to subjects table
+       $page_set = find_pages_for_subjects($subject["id"]);
+				$output .= '<ul class="pages">';
+         while($page = mysqli_fetch_assoc($page_set)){
+            $output .= "<li";
+            if($page["id"] == $page_id){
+            $output .= " class=\"selected\"";
+            }
+            else{
+     $output .= ">";
+            }
+							$output .= '<a href="manage_content.php?page=';
+							$output .= urlencode($page["id"]);
+							$output .= 	'">';
+								$output .= $page["menu_name"];
+							$output .= '</a>';
+							$output .= '</li>';
+      }
+							 mysqli_free_result($page_set);
+				$output .= '</ul>';
+				$output .= '</li>';
+
+    }
+          // 4 reslese retun data
+     mysqli_free_result($subject_set);
+$output .= '</ul>';
+return $output;
+}
+
+?>
--- public/manage_content.php
+++ public/manage_content.php
@@ -1,57 +1,33 @@
 <?php
 // include once tym adding db-connection
 require_once("../include/db_connection.php"); ?>
-
 <?php
 // include once tym
 require_once("../include/functions.php"); ?>
 <?php include("../include/layouts/header.php"); ?>
+
+<?php
+$selected_page_id  = null;
+$selected_subject_id  = null;
+if(isset($_GET["subject"])){
+	$selected_subject_id = $_GET["subject"];
+}elseif(isset($_GET["page"])){
+	$selected_page_id = $_GET["page"];
+}
+ ?>
+
 <div id="main">
 	<div id="navigation">
-		<ul class="subjects">
-			<?php
-			// print subject by creating function see function.php
-			$subject_set =  find_all_subjects();
-			?>
-      <?php
-      //3. return data if any key are <integ></integ>er  mysqli_fetch_row
-      // key are coloumn result are in an associative array mysqli_fetch_assoc
-      // result in either or both typer of arrays mysqli_fetch_assoc
-      //while($row = mysqli_fetch_row($result)){
-      while($subject = mysqli_fetch_assoc($subject_set)){
-      ?>
-      <li>
-				 <?php echo $subject["menu_name"]; ?>
-				 <?php
-				 // function find pages belong to subjects subjects["id"] is belong to subjects table 
-				 $page_set = find_pages_for_subjects($subject["id"]);
-	 			?>
-				 <ul class="pages">
-					 <?php
-					 while($page = mysqli_fetch_assoc($page_set)){
-	 	      ?>
-					<li>
-						<?php echo $page["menu_name"]; ?>
-					</li>
-					<?php
-				}
-					 ?>
-					 <?php mysqli_free_result($page_set); ?>
-				 </ul>
-			</li>
-
-      <?php
-      }
-       ?>
-			 <?php
-			 // 4 reslese retun data
-			 mysqli_free_result($subject_set);
-			  ?>
-
-    </ul>
+		<?php
+		//function navigation to display function 
+		echo navigation($selected_subject_id,$selected_page_id); ?>
 	</div>
 	<div id="page">
 		<h2>Manage Content</h2>
+		<?php
+		echo $selected_subject_id;
+		echo $selected_page_id;
+		 ?>
 	</div>
 </div>
 <?php include("../include/layouts/footer.php"); ?>

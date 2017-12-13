6be9d506e2576aa4edf778d6994a1843417fa9f3
kratos619 <gaurav.palaspagar@hotmail.com>
2017-12-13 17:20:42 +0530

    single page submission

--- public/edit_subject.php
+++ public/edit_subject.php
@@ -10,6 +10,45 @@ require_once("../include/functions.php"); ?>
 <?php if(!$selected_subject_id){
   redirect_to("manage_content.php");
 } ?>
+
+<?php
+if(isset($_POST['submit'])){
+
+  if(empty($errors)){
+// perform update
+
+$id = $current_subject["id"];
+  $menu_name = mysql_prep( $_POST["menu_name"]);
+  $position = (int) $_POST["position"];
+  $visible = (int) $_POST["visible"];
+
+  // 2. perform quey
+  $query = "update subjects SET ";
+  $query .= " menu_name = '{$menu_name}', ";
+  $query .= "position = '{$position}', ";
+   $query .= "visible = '{$visible}'";
+  $query .= " where id = {$id} ";
+  $query .= "limit 1";
+  $result = mysqli_query($connection, $query);
+  // test if there was a error
+  if($result && mysqli_affected_rows($connection) == 1){
+    $_SESSION["message"] = "Subject Updated";
+      redirect_to("manage_content.php");
+  }else{
+    //failure
+    $message = "Subject Updated failed";
+  }
+}
+}else{
+
+}//end if(isset($_POST['submit'])){
+
+ ?>
+
+
+<?php
+// include validation function file
+require_once("../include/validation_functions.php"); ?>
 <?php include("../include/layouts/header.php"); ?>
 
 <div id="main">
@@ -20,8 +59,10 @@ require_once("../include/functions.php"); ?>
 	</div>
 	<div id="page">
 <?php
-// displaying successfull or not message
-echo message();
+// displaying successfull or not message just use variable not session
+if(!empty($message)){
+  echo "<div class=\"message\">" . $message . "</div>";
+}
 	?>
 	<?php
 // displayig error messe
@@ -30,14 +71,26 @@ echo message();
 	?>
 
 <h2>Edit Subject: <?php echo $current_subject["menu_name"]; ?> </h2>
-<form action="edit_subject.php" method="post">
+<form action="edit_subject.php?subject=<?php echo $current_subject["id"]; ?>" method="post">
   <p>Edit Name:
   <input type="text" name="menu_name" value="<?php echo  $current_subject["menu_name"]; ?>">
   </p>
-	<p>
+  <p>
 		Position:
-		<select class="" name="position" >
-      <option><?php echo $current_subject["position"]; ?></option>
+		<select class="" name="position">
+			<?php
+			$subject_set = find_all_subjects();
+			// mysqli_num_rows retun number of rows
+			$subject_count = mysqli_num_rows($subject_set);
+				for ($count=1; $count <= ($subject_count + 1); $count++) {
+					echo "<option value=\"{$count}\"";
+            if($current_subject["position"] == $count){
+                  echo " selected";
+            }
+          echo ">{$count}</option>";
+				}
+			 ?>
+
 		</select>
 	</p>
   <p>visible:
